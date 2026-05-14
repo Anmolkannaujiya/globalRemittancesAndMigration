d04a045b75329b8e16813ac3b62ad45bf0f02646
anmolkannaujiya <anmolkannaujiya@outlook.com>
2026-05-14 21:23:22 +0530

    initialising the project

---
+++ exploratory.py
@@ -0,0 +1,17 @@
+#++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+#+ (****    PROJECT ON EXPLORATORY DATA ANALYSIS ON THE DATASET OF GLOBAL REMITTANCES   ****) +
+#++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+# ============================================================
+# Loading the datasets and library
+# ============================================================
+
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+import warnings
+
+#ignoring the warnings
+warnings.filterwarnings("ignore")
+
